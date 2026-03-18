64eac4b1d37328a104a09e840d4ed93828e50ff5
Marc <marc.doumit@insa-lyon.fr>
2026-03-18 13:29:43 +0100

    deleted mockup and fixed view

--- Harmony/HarmonyBack/views.py
+++ Harmony/HarmonyBack/views.py
@@ -4,14 +4,13 @@ from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer, RegisterSerializer
 from .models import User
-from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class RegisterView(APIView):
+class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
