14a92ba138e0f169fca7d98bf8be4224610f0450
Hugo MULLER <hugo.muller@insa-lyon.fr>
2026-03-25 13:03:59 +0100

    resolve issue with Radar chart glittering

--- Harmony/frontend/src/components/UserStatChart.js
+++ Harmony/frontend/src/components/UserStatChart.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
 
 function UserStatsChart({ stats }) {
+  const memoizedChart = useMemo(() => {
+        if (!stats || stats.length === 0) return null;
   return (
     <div style={{ width: '100%', height: 200, backgroundColor: '#2f3136', borderRadius: '4px' }}>
       <ResponsiveContainer>
@@ -19,6 +21,12 @@ function UserStatsChart({ stats }) {
       </ResponsiveContainer>
     </div>
   );
+  },[stats]);
+  return (
+        <div style={{ width: '100%', height: '220px', marginTop: '10px' }}>
+            {memoizedChart || <p>Chargement...</p>}
+        </div>
+    );
 }
 
 export default UserStatsChart;
