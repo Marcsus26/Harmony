ad5273ed416aa8b2a92403dbe0d4039b0f7930f7
aymen <aymen.hachemi@insa-lyon.fr>
2026-03-27 17:02:47 +0100

    added friend suggestions

--- Harmony/HarmonyBack/urls.py
+++ Harmony/HarmonyBack/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserView, RegisterView, CurrentUserView, ProfileUpdateView, GameSuggestionsView, UserServersView, ServerCreateView, ServerChannelsView, ChannelMessagesView, ChannelCreateView, MessageCreateView, ServerUpdateMembersView, UserServersView, ServerCreateView, ServerChannelsView, ChannelMessagesView, ChannelCreateView, MessageCreateView, ServerUpdateMembersView, ServerDeleteView, ServerLeaveView, RefuseGameView,LikeGameView,LikeGameView, UserStatsView, SteamGameDetailsView, SendFriendRequestView, PendingRequestsListView, RespondToRequestView, FriendsListView, FriendsUserStatsView, SuggestedPlayersView, FriendsUserStatsView
+from .views import UserView, RegisterView, CurrentUserView, ProfileUpdateView, GameSuggestionsView, UserServersView, ServerCreateView, ServerChannelsView, ChannelMessagesView, ChannelCreateView, MessageCreateView, ServerUpdateMembersView, UserServersView, ServerCreateView, ServerChannelsView, ChannelMessagesView, ChannelCreateView, MessageCreateView, ServerUpdateMembersView, ServerDeleteView, ServerLeaveView, RefuseGameView,LikeGameView,LikeGameView, UserStatsView, SteamGameDetailsView, SendFriendRequestView, PendingRequestsListView, RespondToRequestView, FriendsListView, FriendsUserStatsView, SuggestedPlayersView, FriendsUserStatsView, SuggestedPlayersView
 
 urlpatterns = [
     path('', UserView.as_view()),
@@ -30,4 +30,5 @@ urlpatterns = [
     path('recommendations/friends-stats/', FriendsUserStatsView.as_view(), name='friend-stats'),
     path('players/suggested/', SuggestedPlayersView.as_view(), name='suggested_players'),
     path('recommendations/friends-stats/', FriendsUserStatsView.as_view(), name='friend-stats'),
+    path('players/suggested/', SuggestedPlayersView.as_view(), name='suggested_players'),
 ]
